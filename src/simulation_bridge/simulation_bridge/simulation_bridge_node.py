3f868431415432e140d9c161fa3588b2dcb2047f
Tom Jacobs <tomjacobs2003@hotmail.com>
2024-03-08 09:33:55 +0100

    Improve odom readout

--- src/simulation_bridge/simulation_bridge/simulation_bridge_node.py
+++ src/simulation_bridge/simulation_bridge/simulation_bridge_node.py
@@ -8,9 +8,6 @@ import time
 
 class SimulationBridge(Node):
 
-    old_joy_state = None
-    minimum_joystick_change = 0.02
-
     def __init__(self):
         super().__init__('simulation_bridge')
         # Set up publisher
@@ -30,18 +27,40 @@ class SimulationBridge(Node):
         self.start()
 
     def _odometry(self, msg):
-        self.logger.info("Odometry:")
-        self.logger.info(str(msg.pose.pose.orientation.x))
-        self.logger.info(str(msg.pose.pose.orientation.y))
-        self.logger.info(str(msg.pose.pose.orientation.z))
+        self.logger.info("\n\n----- Odometry -----\n")
+
+        # Position
+        position = msg.pose.pose.position
+        self.logger.info("Position:")
+        self.logger.info(f"  x: {position.x:.3f}")
+        self.logger.info(f"  y: {position.y:.3f}")
+        self.logger.info(f"  z: {position.z:.3f}")
+
+        # Orientation (quaternion)
+        orientation = msg.pose.pose.orientation
+        self.logger.info("Orientation (quaternion):")
+        self.logger.info(f"  x: {orientation.x:.3f}")
+        self.logger.info(f"  y: {orientation.y:.3f}")
+        self.logger.info(f"  z: {orientation.z:.3f}")
+        self.logger.info(f"  w: {orientation.w:.3f}")
+
+        # Linear Velocity
+        linear_velocity = msg.twist.twist.linear
+        self.logger.info("Linear Velocity:")
+        self.logger.info(f"  x: {linear_velocity.x:.3f}")
+        self.logger.info(f"  y: {linear_velocity.y:.3f}")
+        self.logger.info(f"  z: {linear_velocity.z:.3f}")
+
+        # Angular Velocity
+        angular_velocity = msg.twist.twist.angular
+        self.logger.info("Angular Velocity:")
+        self.logger.info(f"  x: {angular_velocity.x:.3f}")
+        self.logger.info(f"  y: {angular_velocity.y:.3f}")
+        self.logger.info(f"  z: {angular_velocity.z:.3f}")
+
 
     def start(self):
-        cmd = Twist()
-        cmd.linear.x = -0.0
-
-        self.logger.info("Publishing: " + str(cmd.linear.x))
-        self.vel_pub.publish(cmd)
-        
+        self.logger.info("Starting")
 
 
 def main(args=None):
