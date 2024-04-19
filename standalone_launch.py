754caa38aafb615876899dd6c398e1f0991d56ec
Tom Jacobs <tomjacobs2003@hotmail.com>
2024-04-19 16:21:43 +0200

    Fix shutdown in standalone launch

--- standalone_launch.py
+++ standalone_launch.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import signal
 from colorama import Fore
 from ament_index_python.packages import get_package_share_directory
@@ -9,29 +10,26 @@ import launch_ros
 import subprocess
 from time import sleep, time
 
+scenario_count = 2
+
 def start():
     signal.signal(signal.SIGINT, signal_handler)
 
     timeout_time = 6
     pkg_share = get_package_share_directory('validate')
     validate_launch_file = 'run_scenario.launch.py'
-    goals_file = os.path.join(pkg_share, 'params/robot_goals/scenario1.yaml')
-    config_file = os.path.join(pkg_share, 'params/agent_goals/scenario1.yaml')
+    scenario_file_name = 'scenario' + str(scenario_count) + '.yaml'
+    goals_file = os.path.join(pkg_share, 'params/robot_goals/', scenario_file_name)
+    config_file = os.path.join(pkg_share, 'params/agent_goals/', scenario_file_name)
+
+    kill_all()
 
     run_scenarios(validate_launch_file, goals_file, config_file, timeout_time)
 
 def run_scenarios(validate_launch_file, goals_file, config_file, timeout_time):
     while True:
         evaluation_timeout_timer = 0
-
-        # Kill any existing gzserver and gzclient
-        print(Fore.GREEN, "Killing existing processes and waiting...")
-        subprocess.run(['killall', '-w', '-KILL', 'gzserver'])
-        subprocess.run(['killall', '-w', '-KILL', 'gzclient'])
-        subprocess.run(['killall', '--process-group', '-w', '-KILL', 'ros2'])
-        subprocess.run(['killall', '-w', '-KILL', 'rviz2'])
-
-        sleep(2)
+        sleep(1)
 
         print("Launching scenario")
         # Pass the goals file and config file to the launch file
@@ -57,9 +55,10 @@ def monitor_scenario_completion(proc, evaluation_timeout_timer, timeout_time):
                 else:
                     print(Fore.RED, "Scenario failed with exit code ", poll)
                 break
-
+        
         line = line.decode('utf-8').strip()
-        print(line)
+
+        print(Fore.WHITE, line)
 
         if 'Calling evaluation service...' in line:
             print('Waiting for evaluation to finish...')
@@ -67,13 +66,17 @@ def monitor_scenario_completion(proc, evaluation_timeout_timer, timeout_time):
 
         if 'Scenario failed, shutting down...' in line:
             print(Fore.RED, "Scenario failed.")
-            end_scenario(proc)
+            print(Fore.GREEN, "Launching same scenario...")
+            terminate_process()
             break
 
         elif 'Scenario complete, shutting down...' in line:
             sleep(1)
             print('Killing...')
             end_scenario(proc)
+            print(Fore.GREEN, "Scenario completed successfully.")
+            print(Fore.GREEN, "Launching next scenario...")
+            scenario_count += 1
             break
 
         if evaluation_timeout_timer > 0 and time() - evaluation_timeout_timer > timeout_time:
@@ -88,38 +91,42 @@ def end_scenario(proc):
     # Get file: safety_metrics.txt
     print(Fore.GREEN, "Getting safety metrics...")
 
-    path = os.path.dirname(os.path.abspath(__file__))
+    path = Path.cwd()
+    Path(os.path.join(path, "scenario_metrics")).mkdir(parents=True, exist_ok=True)
 
-    with open(os.path.abspath(os.path.join(path, '/safety_metrics.txt')), 'r') as file:
-        with open(os.path.abspath(os.path.join(path, 'metrics_scenario1.txt')), 'w') as metrics_file:
+    scenario_metrics_file = os.path.join(path, 'scenario_metrics/metrics_scenario' + str(scenario_count) + '.txt')
+    print(Fore.YELLOW, f'Saving metrics for scenario {scenario_count} in {scenario_metrics_file}')
+
+    with open(os.path.join(path, 'safety_metrics.txt'), 'r') as file:
+        with open(os.path.join(path, scenario_metrics_file), 'w') as metrics_file:
             metrics_file.write(file.read())
             metrics_file.write('\n\n\n')
             metrics_file.close()
     
-    terminate_process(proc)
+    terminate_process()
 
 
-def terminate_process(proc):
+def terminate_process():
     shutdown_timeout_timer = time()
-    shutdown_timeout_time = 10
+    shutdown_timeout_time = 15
 
     print(Fore.RED, "Killing...")
-    proc.send_signal(subprocess.signal.SIGINT)
-    while proc.poll() is None:
-        sleep(1)
-        proc.send_signal(subprocess.signal.SIGINT)
-        if time() - shutdown_timeout_timer > shutdown_timeout_time:
-            print(Fore.RED, "Failed to shutdown, killing...")
-            proc.send_signal(subprocess.signal.SIGTERM)
-            break
+
+    kill_all()
 
 def signal_handler(sig, frame):
-    print(Fore.RED, "Received INT signal, killing...")
+    print(Fore.RED, "Ctrl+C detected, killing...")
+    kill_all()
+    exit(0)
+    
+
+def kill_all():
+    print(Fore.GREEN, "Killing existing processes and waiting...")
     subprocess.run(['killall', '-w', '-KILL', 'gzserver'])
-    subprocess.run(['killall', '-w', '-KILL', 'gzclient'])
     subprocess.run(['killall', '--process-group', '-w', '-KILL', 'ros2'])
     subprocess.run(['killall', '-w', '-KILL', 'rviz2'])
-    exit(0)
+    subprocess.run(['killall', '-w', '-KILL', 'gzclient'])
+    sleep(2)
 
 
 if __name__ == '__main__':
